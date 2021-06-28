0fc39b2fce13578d6b69ddc1fa0111e8fb9bb874
Alina Orozco <alina.orozco12@gmail.com>
2021-06-28 11:54:32 +0200

    Removing 0 module step

--- 0_DataPrepCleanup.sh
+++ 0_DataPrepCleanup.sh
@@ -46,27 +46,27 @@
 	fi
 
 
-# Set Working Directory
+# Get Working Directory
 # -------------------------------------------------
 echo
-echo Changing to Working Directory
+echo Current Working Directory
 echo ----------------------------------------------
-echo ${WorkingDir}
+echo $PWD
 
-	cd ${WorkingDir}
+#Get data directory
+read -p "Location for PLINK bim file": 
 
-	
-#Get the BaseName of the Data Placed in the PLACE_DATA_2B_FIXED_HERE -- must have a Plink .bim file in the folder
-RawData="$(ls ./0_DataPrepModule/PLACE_DATA_2B_FIXED_HERE/*.bim | awk -F/ '{print $NF}' | awk -F'.' '{print $1}')"
+#Get the BaseName of the Data -- must have a Plink .bim file in the folder
+RawData="$(ls $REPLY/*.bim | awk -F/ '{print $NF}' | awk -F'.' '{print $1}')"
 
 # Controls whether BCFTools +Fixref is performed on the dataset
 
 if [ "${PerformFixref,,}" == "t" ]; then
-	echo "Performing BCFTools +Fixref on dataset in ./0_DataPrepModule/PLACE_DATA_2B_FIXED_HERE/"
+	echo "Performing BCFTools +Fixref on dataset"
 		echo ----------------------------------------------
 		
 	#Make Temp Directory in which all Temp files will populate
-		mkdir -p ./0_DataPrepModule/TEMP
+		mkdir -p $PWD/TMP
 
 	# Download all the Reference Data to Reformat the files
 	# ----------------------------------------------------------------------------
@@ -79,23 +79,23 @@ if [ "${PerformFixref,,}" == "t" ]; then
 	
 		echo Downloading: ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/human_g1k_v37.fasta.gz
 		echo
-		wget --directory-prefix=./0_DataPrepModule/RefAnnotationData/ ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/human_g1k_v37.fasta.gz
-		wget --directory-prefix=./0_DataPrepModule/RefAnnotationData/ ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/human_g1k_v37.fasta.fai	
+		wget --directory-prefix=./RefAnnotationData/ ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/human_g1k_v37.fasta.gz
+		wget --directory-prefix=./RefAnnotationData/ ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/reference/human_g1k_v37.fasta.fai	
 		
 		# Unzip Fasta file
-		${gunzip_Exec} -d ./0_DataPrepModule/RefAnnotationData/human_g1k_v37.fasta.gz
+		${gunzip_Exec} -d ./RefAnnotationData/human_g1k_v37.fasta.gz
 		
 		# Rezip Fasta File in bgzip
-		${bgzip_Exec} ./0_DataPrepModule/RefAnnotationData/human_g1k_v37.fasta
-		rm ./0_DataPrepModule/RefAnnotationData/human_g1k_v37.fasta
+		${bgzip_Exec} ./RefAnnotationData/human_g1k_v37.fasta
+		rm ./RefAnnotationData/human_g1k_v37.fasta
 		
 	
 	# Download the annotation files (make sure the the build version is correct) to flip/fix the alleles
 		echo
 		echo Downloading ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606_b150_GRCh37p13/VCF/All_20170710.vcf.gz
 		echo
-		wget --directory-prefix=./0_DataPrepModule/RefAnnotationData/ ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606_b150_GRCh37p13/VCF/All_20170710.vcf.gz
-		wget --directory-prefix=./0_DataPrepModule/RefAnnotationData/ ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606_b150_GRCh37p13/VCF/All_20170710.vcf.gz.tbi
+		wget --directory-prefix=./RefAnnotationData/ ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606_b150_GRCh37p13/VCF/All_20170710.vcf.gz
+		wget --directory-prefix=./RefAnnotationData/ ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606_b150_GRCh37p13/VCF/All_20170710.vcf.gz.tbi
 	
 	
 	fi
@@ -115,7 +115,7 @@ if [ "${PerformFixref,,}" == "t" ]; then
 		echo
 		echo
 	
-		${Plink_Exec} --bfile ./0_DataPrepModule/PLACE_DATA_2B_FIXED_HERE/$RawData --recode vcf --out ./0_DataPrepModule/TEMP/DataFixStep1_${RawData}
+		${Plink_Exec} --bfile ./$RawData --recode vcf --out ./TEMP/DataFixStep1_${RawData}
 	
 	# Convert from a VCF into a BCF and also rename the chromosomes to match the reference fasta (where [chr]23 is X, 24 is Y, etc.)
 		
@@ -124,7 +124,7 @@ if [ "${PerformFixref,,}" == "t" ]; then
 		echo
 		echo
 	
-		bcftools annotate -Ob --rename-chrs ./0_DataPrepModule/RefAnnotationData/PlinkChrRename.txt ./0_DataPrepModule/TEMP/DataFixStep1_${RawData}.vcf > ./0_DataPrepModule/TEMP/DataFixStep1_${RawData}.bcf
+		bcftools annotate -Ob --rename-chrs ./RefAnnotationData/PlinkChrRename.txt ./TEMP/DataFixStep1_${RawData}.vcf > ./TEMP/DataFixStep1_${RawData}.bcf
 	
 	fi
 	
@@ -142,7 +142,7 @@ if [ "${PerformFixref,,}" == "t" ]; then
 		echo
 		echo
 	
-		bcftools +fixref ./0_DataPrepModule/TEMP/DataFixStep1_${RawData}.bcf -- -f ./0_DataPrepModule/RefAnnotationData/human_g1k_v37.fasta.gz
+		bcftools +fixref ./TEMP/DataFixStep1_${RawData}.bcf -- -f ./RefAnnotationData/human_g1k_v37.fasta.gz
 	
 	# Run bcftools to fix/swap the allels based on the downloaded annotation file
 		printf "\n\nRun bcftools +fixref to fix allels based on the downloaded annotation file \n"
@@ -150,7 +150,7 @@ if [ "${PerformFixref,,}" == "t" ]; then
 		echo
 		echo
 	
-		bcftools +fixref ./0_DataPrepModule/TEMP/DataFixStep1_${RawData}.bcf -Ob -o ./0_DataPrepModule/TEMP/DataFixStep2_${RawData}-RefFixed.bcf -- -d -f ./0_DataPrepModule/RefAnnotationData/human_g1k_v37.fasta.gz -i ./0_DataPrepModule/RefAnnotationData/All_20170710.vcf.gz
+		bcftools +fixref ./TEMP/DataFixStep1_${RawData}.bcf -Ob -o ./TEMP/DataFixStep2_${RawData}-RefFixed.bcf -- -d -f ./RefAnnotationData/human_g1k_v37.fasta.gz -i ./0_DataPrepModule/RefAnnotationData/All_20170710.vcf.gz
 	
 	# Rerun the bcftool +fixref check to see if the file has been fixed and all unmatched alleles have been dropped
 		printf "\n\nRun bcftools +fixref to see if the file has been fixed - all alleles are matched and all unmatched alleles have been dropped \n"
@@ -158,7 +158,7 @@ if [ "${PerformFixref,,}" == "t" ]; then
 		echo
 		echo
 	
-		bcftools +fixref ./0_DataPrepModule/TEMP/DataFixStep2_${RawData}-RefFixed.bcf -- -f ./0_DataPrepModule/RefAnnotationData/human_g1k_v37.fasta.gz
+		bcftools +fixref ./TEMP/DataFixStep2_${RawData}-RefFixed.bcf -- -f ./RefAnnotationData/human_g1k_v37.fasta.gz
 		
 	fi
 	
@@ -173,7 +173,7 @@ if [ "${PerformFixref,,}" == "t" ]; then
 		printf "\n\nSorting the BCF output since fixing it may have made it unsorted \n"
 		echo ----------------------------------------------
 	
-		(bcftools view -h ./0_DataPrepModule/TEMP/DataFixStep2_${RawData}-RefFixed.bcf; bcftools view -H ./0_DataPrepModule/TEMP/DataFixStep2_${RawData}-RefFixed.bcf | sort -k1,1d -k2,2n;) | bcftools view -Ob -o ./0_DataPrepModule/TEMP/DataFixStep3_${RawData}-RefFixedSorted.bcf
+		(bcftools view -h ./TEMP/DataFixStep2_${RawData}-RefFixed.bcf; bcftools view -H ./TEMP/DataFixStep2_${RawData}-RefFixed.bcf | sort -k1,1d -k2,2n;) | bcftools view -Ob -o ./TEMP/DataFixStep3_${RawData}-RefFixedSorted.bcf
 		
 		printf "Done \n\n\n"
 	
@@ -183,7 +183,7 @@ if [ "${PerformFixref,,}" == "t" ]; then
 		echo
 		echo
 	
-		${Plink_Exec} --bcf ./0_DataPrepModule/TEMP/DataFixStep3_${RawData}-RefFixedSorted.bcf --make-bed --out ./0_DataPrepModule/TEMP/DataFixStep3_${RawData}-RefFixSorted
+		${Plink_Exec} --bcf ./TEMP/DataFixStep3_${RawData}-RefFixedSorted.bcf --make-bed --out ./TEMP/DataFixStep3_${RawData}-RefFixSorted
 		
 		
 	# Finally Remove any positional duplicates 
@@ -195,17 +195,17 @@ if [ "${PerformFixref,,}" == "t" ]; then
 		echo
 		echo
 	
-		${Plink_Exec} --bfile ./0_DataPrepModule/TEMP/DataFixStep3_${RawData}-RefFixSorted --list-duplicate-vars ids-only suppress-first --out ./0_DataPrepModule/TEMP/Dups2Remove
+		${Plink_Exec} --bfile ./TEMP/DataFixStep3_${RawData}-RefFixSorted --list-duplicate-vars ids-only suppress-first --out ./TEMP/Dups2Remove
 		
 		# Report Number of duplicates:
-		DuplicateNumber="$(wc ./0_DataPrepModule/TEMP/Dups2Remove.dupvar | awk '{print $1}')"
+		DuplicateNumber="$(wc ./TEMP/Dups2Remove.dupvar | awk '{print $1}')"
 		
 		printf "\n\nRemoving Positional and Allelic Duplicates if they exist\nFound ${DuplicateNumber} Duplicate Variant/s\n"
 		echo ----------------------------------------------
 		echo
 		echo
 	
-		${Plink_Exec} --bfile ./0_DataPrepModule/TEMP/DataFixStep3_${RawData}-RefFixSorted --exclude ./0_DataPrepModule/TEMP/Dups2Remove.dupvar --make-bed --out ./0_DataPrepModule/TEMP/DataFixStep4_${RawData}-RefFixSortedNoDups
+		${Plink_Exec} --bfile ./TEMP/DataFixStep3_${RawData}-RefFixSorted --exclude ./TEMP/Dups2Remove.dupvar --make-bed --out ./TEMP/DataFixStep4_${RawData}-RefFixSortedNoDups
 		
 		
 		
@@ -216,7 +216,7 @@ if [ "${PerformFixref,,}" == "t" ]; then
 		echo
 		echo
 		
-		${Plink_Exec} --bfile ./0_DataPrepModule/TEMP/DataFixStep4_${RawData}-RefFixSortedNoDups --update-sex ./0_DataPrepModule/PLACE_DATA_2B_FIXED_HERE/${RawData}.fam 3 --make-bed --out ./1_Target/PLACE_NEW_PROJECT_TARGET_DATA_HERE/DataFixStep5_${RawData}-PhaseReady
+		${Plink_Exec} --bfile ./TEMP/DataFixStep4_${RawData}-RefFixSortedNoDups --update-sex ./PLACE_DATA_2B_FIXED_HERE/${RawData}.fam 3 --make-bed --out ./1_Target/PLACE_NEW_PROJECT_TARGET_DATA_HERE/DataFixStep5_${RawData}-PhaseReady
 		
 
 		echo
@@ -239,7 +239,7 @@ if [ "${PerformFixref,,}" == "t" ]; then
 		echo Tidying Up -- Cleanup Intermediate Files
 		echo ----------------------------------------------
 	
-		if [ -d "./0_DataPrepModule/TEMP" ]; then rm -r ./0_DataPrepModule/TEMP; fi
+		if [ -d "./TEMP" ]; then rm -r ./TEMP; fi
 
 		#rm ./0_DataPrepModule/DataFixStep*
 	
@@ -248,7 +248,7 @@ if [ "${PerformFixref,,}" == "t" ]; then
 elif [ "${PerformFixref,,}" == "f" ]; then
 
 	#Make Temp Directory in which all Temp files will populate
-		mkdir -p ./0_DataPrepModule/TEMP
+		mkdir -p ./TEMP
 
 		
 	# Finally Remove any positional duplicates 
@@ -259,14 +259,14 @@ elif [ "${PerformFixref,,}" == "f" ]; then
 		echo
 		echo
 	
-		${Plink_Exec} --bfile ./0_DataPrepModule/PLACE_DATA_2B_FIXED_HERE/${RawData} --list-duplicate-vars ids-only suppress-first --out ./0_DataPrepModule/TEMP/Dups2Remove
+		${Plink_Exec} --bfile ./PLACE_DATA_2B_FIXED_HERE/${RawData} --list-duplicate-vars ids-only suppress-first --out ./TEMP/Dups2Remove
 		
 		printf "\n\nRemoving Positional and Allelic Duplicates \n"
 		echo ----------------------------------------------
 		echo
 		echo
 	
-		${Plink_Exec} --bfile ./0_DataPrepModule/PLACE_DATA_2B_FIXED_HERE/${RawData} --exclude ./0_DataPrepModule/TEMP/Dups2Remove.dupvar --make-bed --out ./1_Target/PLACE_NEW_PROJECT_TARGET_DATA_HERE/DataFixStep5_${RawData}-PhaseReady
+		${Plink_Exec} --bfile ./PLACE_DATA_2B_FIXED_HERE/${RawData} --exclude ./TEMP/Dups2Remove.dupvar --make-bed --out ./1_Target/PLACE_NEW_PROJECT_TARGET_DATA_HERE/DataFixStep5_${RawData}-PhaseReady
 		
 	if [ "${SaveDataPrepIntermeds,,}" == "f" ]; then
 	
@@ -275,7 +275,7 @@ elif [ "${PerformFixref,,}" == "f" ]; then
 		echo Tidying Up -- Cleanup Intermediate Files
 		echo ----------------------------------------------
 	
-		if [ -d "./0_DataPrepModule/TEMP" ]; then rm -r ./0_DataPrepModule/TEMP; fi
+		if [ -d "./TEMP" ]; then rm -r ./TEMP; fi
 		#rm ./0_DataPrepModule/DataFixStep*
 	
 	fi
